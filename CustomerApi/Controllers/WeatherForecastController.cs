da2f6724b553e69caea9897b90e61310cadfd9e2
Karan Taneja <golgappaaaaa@gmail.com>
2026-01-19 23:29:24 +0530

    automatic checkingsgdd

--- CustomerApi/Controllers/WeatherForecastController.cs
+++ CustomerApi/Controllers/WeatherForecastController.cs
@@ -8,6 +8,6 @@ public class WeatherForecastController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get() => Ok(new[] { "Customer A", "Customer B" });
-    [HttpGet]
+    [HttpGet("test-deploy")]
     public IActionResult CheckautomaticdeplomenttillrenderGet() => Ok(new[] { "dCheckautomaticdeplomenttillrenderGet A", "CheckautomaticdeplomenttillrenderGet B" });
 }
